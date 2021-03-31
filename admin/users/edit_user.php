074c3e1f086bca5b4bb020fe55b6ca8aaa6dd6a6
Lucas van Wijk <200519@student.glu.nl>
2021-03-31 15:21:01 +0200

    WIP users CMS in admin

---
+++ admin/users/edit_user.php
@@ -0,0 +1,8 @@
+<?php
+    include('../core/header.php');
+    include('../core/checklogin_admin.php');
+?>
+wijzig pagina 
+<?php
+    include('../core/footer.php');
+?>
--- core/db_connect.php
+++ core/db_connect.php
@@ -4,18 +4,18 @@ session_start();
 /**
  * Voor de MAC gebruikers;
  */
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "root";
-$dbname = "webshop";
+// $dbhost = "localhost";
+// $dbuser = "root";
+// $dbpass = "root";
+// $dbname = "webshop";
 
 /**
  * Voor de Windows gebruikers;
  */
-// $dbhost = "localhost";
-// $dbuser = "root";
-// $dbpass = "";
-// $dbname = "webshop";
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "webshop";
 
 $con = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
@@ -24,8 +24,8 @@ if ($con -> connect_errno) {
     exit();
 }
 
-define("BASEURL","http://localhost:8080/webdev-base-webshop/");
-define("BASEURL_CMS","http://localhost:8080/webdev-base-webshop/admin/");
+define("BASEURL","http://localhost/webshop%203/");
+define("BASEURL_CMS","http://localhost/webshop%203/admin/");
 
 function prettyDump ( $var ) {
     echo "<pre>";
